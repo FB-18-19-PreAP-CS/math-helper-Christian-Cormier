d3b9fa2d6f67310b4e48e5cfeaa7fdaf62a75c11
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-09-17 14:05:27 -0500

    distance doctest

--- math_helper_CormierC.py
+++ math_helper_CormierC.py
@@ -2,6 +2,30 @@
 #distance
 #midpoint formula
 #quadratic formula
+#sphere formula
 
-def distance(1x,2x,1y,2y):
+import math
+
+def distance(x,x2,y,y2):
+    d = math.sqrt((x2-x)**2+(y2-y)**2)
+    return d
+    '''
+        returns the distance of 2 points
+        
+        >>> distance(2,4,4,2)
+        math.sqrt(8)
+        
+        >>>distance(5,3,9,7)
+        math.sqrt(8)
+        
+        >>>distance(9,3,10,6)
+        math.sqrt(52)
+    '''
     
+def main():
+    pass
+    
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    #main()
