7c1f8e003532a0e2d80b7ab4c8152cd01ad3f57d
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-09-18 14:11:38 -0500

    fixed distance adn quadratic

--- math_helper_CormierC.py
+++ math_helper_CormierC.py
@@ -7,23 +7,42 @@
 import math
 
 def distance(x,x2,y,y2):
-    d = math.sqrt((x2-x)**2+(y2-y)**2)
-    return d
     '''
         returns the distance of 2 points
         
         >>> distance(2,4,4,2)
-        math.sqrt(8)
+        root 8
         
-        >>>distance(5,3,9,7)
-        math.sqrt(8)
+        >>> distance(5,3,9,7)
+        root 8
         
-        >>>distance(9,3,10,6)
-        math.sqrt(52)
+        >>> distance(9,3,10,6)
+        root 52
     '''
     
+    d = (x2-x)**2+(y2-y)**2
+    ans = int(d)
+    hi = str(ans)
+    print('root ' + hi)
+    
+def quadratic(a,b,c):
+    '''
+    gives the the quadratic form of 3 numbers
+    
+    >>> quadratic(1,2,3)
+    
+    >>> quadratic(2,4,6)
+    
+    >>> quadratic(9,5,12)
+    
+    '''
+    
+    q1 = (-b + math.sqrt(b)**2 - 4(a)(c)) / 2(a)
+    q2 = (-b - math.sqrt(b)**2 - 4(a)(c)) / 2(a)
+    print(q1,q2)
 def main():
-    distance(2,4,4,2)
+    distance(5,3,9,7)
+    distance(9,3,10,6)
     
     
 if __name__ == "__main__":
