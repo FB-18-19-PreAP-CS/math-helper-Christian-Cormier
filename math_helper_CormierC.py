a3e8821eb4c279ed010ec6cd78cf1fab1f1ae033
CORMIER.CHRISTIAN <2210020@fbisd.flourbluffschools.net>
2018-09-25 13:58:20 -0500

    fixes for wes to pull

--- math_helper_CormierC.py
+++ math_helper_CormierC.py
@@ -8,7 +8,7 @@ import math
 
 def distance(x,x2,y,y2):
     '''
-        returns the distance of 2 points in root form
+        returns the unsimplified istance of 2 points in root form
         
         >>> distance(2,4,4,2)
         root 8
@@ -18,12 +18,17 @@ def distance(x,x2,y,y2):
         
         >>> distance(9,3,10,6)
         root 52
+        
+        >>> distance(-2,-6,4,9)
+        root 41
+        
+        >>> distance(10,20,10,30
+        root 500
+        
     '''
     
     d = (x2-x)**2+(y2-y)**2
-    ans = int(d)
-    hi = str(ans)
-    print('root ' + hi)
+    return(f'root {d}')
     
 def quadratic(a,b,c):
     '''
@@ -31,17 +36,17 @@ def quadratic(a,b,c):
     the form ax^2 + bx + c = 0
     
     >>> quadratic(1,2,1)
-    -1.00,-1.00
+    the quadratic is -1.00,-1.00
     >>> quadratic(3,7,-6)
-    .67,-3.00
+    the quadratic is .67,-3.00
     >>> quadratic(6,-7,-3)
-    1.50,-0.33
+    the quadratic is 1.50,-0.33
     '''
     
     q1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
     q2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
 
-    print(f'{q1:.2f},{q2:.2f}')
+    return(f'{q1:.2f},{q2:.2f}')
 
     
 def midpoint(x1,x2,y1,y2):
@@ -60,21 +65,21 @@ def midpoint(x1,x2,y1,y2):
     ans2 = float(y)
     hi = str(ans)
     hi2 = str(ans2)
-    print( '('+ hi + ',' + hi2 + ')')
+    return(f' ( {x:.1f} ) , ( {y:.1f) } ')
     
 def circle(r):
-#    '''
-#    finds the diameter of a circle
-#    
-#    >>>circle(4)
-#    50.24
-#    
-#    >>circle(2)
-#    12.16
-#    
-#    >>>circle(5)
-#    78.5  
-#    '''
+    '''
+    finds the area of a circle using 3.14 for pi
+    
+    >>> circle(4)
+    50.24
+    
+    >>> circle(2)
+    12.16
+    
+    >>> circle(5)
+    78.5  
+    '''
     r2 = r**2 * 3.14
     return(r2)
 
@@ -116,45 +121,37 @@ def main():
         if inp == "1":
             run_dist()
             cont = input("do you want to continue: ")
-            if cont == "yes":
+            if cont.lower() == "yes":
                 main()
-            elif cont == "no":
+            if cont.lower() == "no":
                 break
         elif inp == "2":
             run_quad()
             cont = input("do you want to continue: ")
-            if cont == "yes":
+            if cont.lower() == "yes":
                 main()
-            elif cont == "no":
+            if cont.lower() == "no":
                 break
         elif inp == "3":
             run_mid()
             cont = input("do you want to continue: ")
-            if cont == "yes":
+            if cont.lower() == "yes":
                 main()
-            elif cont == "no":
+            if cont.lower() == "no":
                 break
         elif inp == "4":
             run_circ()
             cont = input("do you want to continue: ")
-            if cont == "yes":
+            if cont.lower() == "yes":
                 main()
-            elif cont == "no":
+            if cont.lower() == "no":
                 break
         else:
             print("thats not an appropriate value")
             main()
             break
         
-#    distance(2,4,4,2)
-#    distance(5,3,9,7)
-#    distance(9,3,10,6)
-#    quadratic(1,2,1)
-#    quadratic(3,7,-6)
-#    quadratic(6,-7,-3)
-#    midpoint(1,1,2,2)
-#    midpoint(4,2,7,2)
-#    midpoint(9,6,8,1)
+
     
     
     
